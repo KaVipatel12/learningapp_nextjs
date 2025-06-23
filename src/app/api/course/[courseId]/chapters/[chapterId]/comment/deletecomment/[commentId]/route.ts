5cc58a06c2861ec28460a0b68fc50b2ef9aaff9f
kush <email@gmail.com>
2025-06-23 11:10:31 +0530

    Solved bugs while deploying

--- src/app/api/course/[courseId]/chapters/[chapterId]/comment/deletecomment/[commentId]/route.ts
+++ src/app/api/course/[courseId]/chapters/[chapterId]/comment/deletecomment/[commentId]/route.ts
@@ -2,44 +2,45 @@ import { connect } from "@/db/dbConfig";
 import { Comment, User } from "@/models/models";
 import { NextRequest, NextResponse } from "next/server";
 
-
-export async function DELETE(req: NextRequest, { params }: { params: { commentId : string}}) {
+export async function DELETE(
+  req: NextRequest,
+  props: { params: Promise<{ commentId: string }> }
+): Promise<NextResponse> {
   await connect();
 
   try {
-    const { commentId } = params;
+    const { commentId } = await props.params;
 
     const findUser = await Comment.findById(commentId); 
-    if(!findUser){
-        return NextResponse.json({msg : "Something went wrong"}, {status : 400})
+    if (!findUser) {
+      return NextResponse.json({ msg: "Something went wrong" }, { status: 400 });
     }
 
-    const userId = findUser.userId.toString(); 
+    const userId = findUser.userId?.toString(); 
 
-    if(!userId){
-        return NextResponse.json({msg : "Something went wrong"}, {status : 400})
+    if (!userId) {
+      return NextResponse.json({ msg: "Something went wrong" }, { status: 400 });
     }
 
-    const pullCommentId = await User.findByIdAndUpdate(userId , {
-        $pull : {
-            comment : commentId
-        }},
-        {new : true}
-    )
+    const pullCommentId = await User.findByIdAndUpdate(
+      userId,
+      { $pull: { comment: commentId } },
+      { new: true }
+    );
 
-    console.log(pullCommentId); 
-    const deleteComment = await Comment.findByIdAndDelete(commentId)
+    console.log("User after pulling comment:", pullCommentId);
 
-    if(!deleteComment){
-        return NextResponse.json({msg : "Something went wrong"}, {status : 400})
+    const deleteComment = await Comment.findByIdAndDelete(commentId);
+    if (!deleteComment) {
+      return NextResponse.json({ msg: "Something went wrong" }, { status: 400 });
     }
 
     return NextResponse.json(
-      { msg: "Comment deleted successfully"},
+      { msg: "Comment deleted successfully" },
       { status: 201 }
     );
   } catch (error) {
-    console.error("Error submitting comment:", error);
+    console.error("Error deleting comment:", error);
     return NextResponse.json(
       { error: "Something went wrong" },
       { status: 500 }
